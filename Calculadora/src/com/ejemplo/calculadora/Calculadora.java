59267b38163295fb2a381eeac4b3ef30ea96edfd
estudiante <estudiante@oretania-PC>
2025-10-17 09:59:28 +0200

    Versión 2: Añadida la operación de restar

--- Calculadora/src/com/ejemplo/calculadora/Calculadora.java
+++ Calculadora/src/com/ejemplo/calculadora/Calculadora.java
@@ -1,11 +1,33 @@
 package com.ejemplo.calculadora;
+import java.util.Scanner;
 
 public class Calculadora {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double a, b;
+		String operador;
+		
+		System.out.print("Introduce un número: ");
+		a = sc.nextDouble();
+		
+		System.out.print("Introduce otro número: ");
+		b = sc.nextDouble();
+		
+		System.out.print("Introduce el operador (+, -, *, /): ");
+		operador = sc.next();
+		
+		if (operador.equals("+")) {
+			System.out.print(sumar(a, b));
+		} else if (operador.equals("-")) {
+			System.out.print(restar(a, b));
+		}
+	}
 	
-	public double sumar(double a, double b) {
+	public static double sumar(double a, double b) {
 		 return a + b;
 	}
-	public double restar(double a, double b) {
+	public static double restar(double a, double b) {
 		 return a - b;
 	}
 }
